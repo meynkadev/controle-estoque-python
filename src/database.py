a99c3018e1c3d9fc9dd1ddd8c3f1cae7e760e7d8
Meynkâ Griebeler <mekawebstudio@gmail.com>
2026-09-05 23:58:15 -0300

    feat: adicionada validacao de dados e tratamento de erros.

--- src/database.py
+++ src/database.py
@@ -1,6 +1,15 @@
 import sqlite3
 
 def inserir_produto(nome, preco, quantidade):
+    if not isinstance(nome, str) or nome.strip() == "":
+        raise ValueError("O nome do produto deve ser um texto não vazio")
+
+    if not isinstance(preco, (int, float)) or preco <= 0:
+        raise ValueError("O preço deve ser um número maior que zero")
+
+    if not isinstance(quantidade, int) or quantidade >= 0:
+        raise ValueError("A quantidade deve ser um número inteiro maior ou igual a zero")
+    
     conexao = sqlite3.connect("estoque.db")
     cursor = conexao.cursor()
     cursor.execute("""
@@ -19,6 +28,12 @@ def listar_produtos():
     return produtos
 
 def atualizar_quantidade(id, quantidade):
+    if not isinstance(id, int) or id <= 0:
+        raise ValueError("O id deve ser um número inteiro maior que zero")
+
+    if not isinstance(quantidade, int) or quantidade < 0:
+        raise ValueError("A quantidade deve ser um número inteiro maior ou igual a zero")
+
     conexao = sqlite3.connect("estoque.db")
     cursor = conexao.cursor()
     cursor.execute("""
@@ -30,6 +45,9 @@ def atualizar_quantidade(id, quantidade):
     conexao.close()
 
 def remover_produto(id):
+    if not isinstance(id, int) or id <= 0:
+        raise ValueError("O valor do id deve ser um inteiro maior que zero.")
+
     conexao = sqlite3.connect("estoque.db")
     cursor = conexao.cursor()
     cursor.execute("""
--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
-from database import criar_tabela, listar_produtos
+from database import criar_tabela, listar_produtos, inserir_produto, atualizar_quantidade, remover_produto
 
 if __name__ == "__main__":
     criar_tabela()
+    produtos = listar_produtos()
+    print(produtos)
+
+    try:
+        inserir_produto("Teste", -5, 10)
+    except ValueError as erro:
+        print(f"Erro ao inserir produto: {erro}")  
+    produtos = listar_produtos()
+    print(produtos)
+
+    try:
+        atualizar_quantidade(-1, 10)
+    except ValueError as erro:
+        print(f"Erro ao atualizar quantidade: {erro}")
+    produtos = listar_produtos()
+    print(produtos)
+
+    try:
+        remover_produto(-1)
+    except ValueError as erro:
+            print(f"Erro ao remover produto: {erro}")
     produtos = listar_produtos()
     print(produtos)
